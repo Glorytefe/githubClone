48ad91cb68345773ad0ad7905afc7f9110f1d32c
Glorytefe <glorytefe@gmail.com>
2021-05-27 14:06:55 +0100

    chore: code cleanup

--- script/index.js
+++ script/index.js
@@ -235,9 +235,9 @@ class UI {
     else if(Math.round(seconds/(60*60)) >= 2) return `updated ${Math.round(seconds/(60*60))} hours ago`;
     else if(Math.round(seconds/(60*60)) >= 1) return `updated 1 hour ago`;
     else if(Math.round(seconds/60) >= 2) return  `updated ${Math.round(seconds/60) } minutes ago`
-    else if(Math.round(seconds/60) >= 1) return `updated  minute ago`;
+    else if(Math.round(seconds/60) >= 1) return `updated  1 minute ago`;
     else if(seconds >= 2)return `updated ${seconds} seconds ago`;
-    else return seconds + `updated 1 second ago`;
+    else return  `updated 1 second ago`;
       }
   
 }
