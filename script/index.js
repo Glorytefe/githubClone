1c005ac3bd604a7c2e00eed1fcfc41708baa9787
Glorytefe <glorytefe@gmail.com>
2021-05-27 05:13:59 +0100

    feat: time update

--- script/index.js
+++ script/index.js
@@ -210,13 +210,25 @@ class UI {
 
   // Date updated
   getNewDateFormat(updated) {
-    const newDate = new Date(updated);
-    let date = newDate.getDate();
-    let month = this.allMonths[newDate.getMonth()];
-    let year = newDate.getFullYear();
-    date = (date < 10) ? `0${date}` :`${date}`
-    return `updated on ${date} ${month}, ${year}`;
-  }
+    
+    let date = new Date(updated)
+    let seconds = Math.floor((new Date() - date) / 1000);
+    if(Math.round(seconds/(60*60*24*365.25)) >= 2) return `updated ${Math.round(seconds/(60*60*24*365.25))} years ago`;
+    else if(Math.round(seconds/(60*60*24*365.25)) >= 1) return `updated 1 year ago`;
+    else if(Math.round(seconds/(60*60*24*30.4)) >= 2) return `updated ${Math.round(seconds/(60*60*24*30.4))} months ago`  ;
+    else if(Math.round(seconds/(60*60*24*30.4)) >= 1) return `updated 1 month ago`;
+    else if(Math.round(seconds/(60*60*24*7)) >= 2) return `updated ${Math.round(seconds/(60*60*24*7)) }  weeks ago`;
+    else if(Math.round(seconds/(60*60*24*7)) >= 1) return `updated 1 week ago`;
+    else if(Math.round(seconds/(60*60*24)) >= 2) return `updated ${Math.round(seconds/(60*60*24))} days ago`;
+    else if(Math.round(seconds/(60*60*24)) >= 1) return `updated 1 day ago`;
+    else if(Math.round(seconds/(60*60)) >= 2) return `updated ${Math.round(seconds/(60*60))} hours ago`;
+    else if(Math.round(seconds/(60*60)) >= 1) return `updated 1 hour ago`;
+    else if(Math.round(seconds/60) >= 2) return  `updated ${Math.round(seconds/60) } minutes ago`
+    else if(Math.round(seconds/60) >= 1) return `1updated  minute ago`;
+    else if(seconds >= 2)return `updated ${seconds} seconds ago`;
+    else return seconds + `updated 1 second ago`;
+      }
+  
 }
 // on load
 document.addEventListener("DOMContentLoaded", () => {
